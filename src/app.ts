b27af7586ceb54a5ada2fa353b1e2384624b44a9
vivek <vivekcse.in@gmail.com>
2025-06-24 14:43:06 +0530

    ddd

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,3 @@
-import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
-
-const app = Fastify({
-    logger: true,
-})
-
-app.get('/', async (_req: FastifyRequest, reply: FastifyReply) => {
-    return reply.status(200).type('text/html').send(html)
-})
 
 
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-    />
-    <title>Vercel + Fastify Template</title>
-  </head>
-  <body>
-    <h1>Hello Server</h1>
-   
-  </body>
-</html>
-`
-
-export default app;
+// export default app;
