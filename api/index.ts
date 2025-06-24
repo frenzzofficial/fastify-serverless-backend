0239e1cd38eb6e7551105da4461c8517753487d2
vivek <vivekcse.in@gmail.com>
2025-06-24 14:27:10 +0530

    git commit

---
+++ api/index.ts
@@ -0,0 +1,34 @@
+import Fastify from 'fastify'
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+
+const app = Fastify({
+    logger: true,
+})
+
+app.get('/', async (req, reply) => {
+    return reply.status(200).type('text/html').send(html)
+})
+
+export default async function handler(req: VercelRequest, reply: VercelResponse) {
+    await app.ready()
+    app.server.emit('request', req, reply)
+}
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
+    />
+    <title>Vercel + Fastify Template</title>
+  </head>
+  <body>
+    <h1>Hello Server</h1>
+   
+  </body>
+</html>
+`
