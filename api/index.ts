610033cc625485071723243b3bf010b0c6b0400a
vivek <vivekcse.in@gmail.com>
2025-06-24 15:13:03 +0530

    commit

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,12 @@
-
-import Fastify from 'fastify'
-import { VercelRequest, VercelResponse } from "@vercel/node"
-const app = Fastify({
-  logger: true,
-})
-
-app.get('/', async (req, reply) => {
-  return reply.status(200).type('text/html').send(html)
-})
+import app from "../src/.js"
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, reply: VercelResponse) {
   await app.ready()
   app.server.emit('request', req, reply)
 }
 
-const html = `
+export const html = `
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -24,44 +16,10 @@ const html = `
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
     />
-    <title>Vercel + Fastify Hello World</title>
-    <meta
-      name="description"
-      content="This is a starter template for Vercel + Fastify."
-    />
+    <title>Fastify Backend</title>
   </head>
   <body>
-    <h1>Vercel + Fastify Hello World</h1>
-    <p>
-      This is a starter template for Vercel + Fastify. Requests are
-      rewritten from <code>/*</code> to <code>/api/*</code>, which runs
-      as a Vercel Function.
-    </p>
-    <p>
-        For example, here is the boilerplate code for this route:
-    </p>
-    <pre>
-<code>import Fastify from 'fastify'
-
-const app = Fastify({
-  logger: true,
-})
-
-app.get('/', async (req, res) => {
-  return res.status(200).type('text/html').send(html)
-})
-
-export default async function handler(req: any, res: any) {
-  await app.ready()
-  app.server.emit('request', req, res)
-}</code>
-    </pre>
-    <p>
-    <p>
-      <a href="https://vercel.com/templates/other/fastify-serverless-function">
-      Deploy your own
-      </a>
-      to get started.
+    <h1>Vercel Fastify template</h1>
   </body>
 </html>
 `
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
 
-console.log("ok");
+import { html } from 'api/index.js';
+import Fastify from 'fastify'
+const app = Fastify({
+    logger: true,
+})
+
+app.get('/', async (req, reply) => {
+    return reply.status(200).type('text/html').send(html)
+})
+
+module.exports = app;
