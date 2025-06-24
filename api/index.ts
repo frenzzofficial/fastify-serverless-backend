10c6ad89b65f8f106e2c0b734e3635c37c154005
vivek <vivekcse.in@gmail.com>
2025-06-24 15:22:41 +0530

    anxah

--- api/index.ts
+++ api/index.ts
@@ -6,21 +6,3 @@ export default async function handler(req: VercelRequest, reply: VercelResponse)
   await app.ready()
   app.server.emit('request', req, reply)
 }
-
-export const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-    />
-    <title>Fastify Backend</title>
-  </head>
-  <body>
-    <h1>Vercel Fastify template</h1>
-  </body>
-</html>
-`
