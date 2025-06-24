a198a98c16640b65202ce58de87508078a9897a3
vivek <vivekcse.in@gmail.com>
2025-06-24 12:00:41 +0530

    test

--- api/server.ts
+++ api/server.ts
@@ -1,9 +1,10 @@
 import { VercelRequest, VercelResponse } from "@vercel/node"
 import app from "../src/app.js"
 
+export default app;
 
 // Export handler for Vercel
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-    await app.ready()
-    app.server.emit("request", req, res)
-}
+// export default async function handler(req: VercelRequest, res: VercelResponse) {
+//     await app.ready()
+//     app.server.emit("request", req, res)
+// }
