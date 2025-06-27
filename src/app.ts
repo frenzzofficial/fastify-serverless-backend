3dd3c8aa70a43f7abe36df6b7691d54a50cc01b3
vivek <vivekcse.in@gmail.com>
2025-06-27 11:49:26 +0530

    test for online

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,51 @@
-// import Fastify from 'fastify';
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
 
-// const app = Fastify({
-//     logger: true,
-// })
+const app = Fastify({
+  logger: false,
+})
 
-// app.get('/', async (req, reply) => {
-//     return reply.status(200).type('text/html').send(html)
-// })
+app.get('/',
+  {
+    schema: {
+      response: {
+        200: {
+          type: 'string',
+        },
+      },
+    },
+  },
+  async (_req: FastifyRequest, reply: FastifyReply) => {
+    return reply.status(200).type('text/html').send(html)
+  });
+app.get('/google',
+  {
+    schema: {
+      response: {
+        200: {
+          type: 'string',
+        },
+      },
+    },
+  },
+  async (_req: FastifyRequest, reply: FastifyReply) => {
+    return reply.status(200).send("google login")
+  });
 
-// export const html = `
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <link
-//       rel="stylesheet"
-//       href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-//     />
-//     <title>Fastify Backend</title>
-//   </head>
-//   <body>
-//     <h1>Vercel Fastify template</h1>
-//   </body>
-// </html>
-// `
-// export default app;
+export const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
+    />
+    <title>Fastify Backend</title>
+  </head>
+  <body>
+    <h1>Vercel Fastify template</h1>
+  </body>
+</html>
+`
+export default app;
